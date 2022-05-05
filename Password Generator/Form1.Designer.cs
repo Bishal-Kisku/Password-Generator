47f77e5dcb7169cf29c133b9019405583aceaa99
Bishal-Kisku <100459920+Bishal-Kisku@users.noreply.github.com>
2022-05-05 13:36:41 +0530

    Add files via upload

---
+++ Password Generator/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace Password_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBoxNumber = new System.Windows.Forms.CheckBox();
+            this.checkBoxSymbol = new System.Windows.Forms.CheckBox();
+            this.checkBoxUpperCase = new System.Windows.Forms.CheckBox();
+            this.checkBoxLowerCase = new System.Windows.Forms.CheckBox();
+            this.button = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.list = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Length";
+            // 
+            // checkBoxNumber
+            // 
+            this.checkBoxNumber.AutoSize = true;
+            this.checkBoxNumber.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxNumber.Location = new System.Drawing.Point(13, 46);
+            this.checkBoxNumber.Name = "checkBoxNumber";
+            this.checkBoxNumber.Size = new System.Drawing.Size(84, 22);
+            this.checkBoxNumber.TabIndex = 2;
+            this.checkBoxNumber.Text = "Numbers";
+            this.checkBoxNumber.UseVisualStyleBackColor = true;
+            this.checkBoxNumber.CheckedChanged += new System.EventHandler(this.checkBoxNumber_CheckedChanged);
+            // 
+            // checkBoxSymbol
+            // 
+            this.checkBoxSymbol.AutoSize = true;
+            this.checkBoxSymbol.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxSymbol.Location = new System.Drawing.Point(191, 46);
+            this.checkBoxSymbol.Name = "checkBoxSymbol";
+            this.checkBoxSymbol.Size = new System.Drawing.Size(79, 22);
+            this.checkBoxSymbol.TabIndex = 3;
+            this.checkBoxSymbol.Text = "Symbols";
+            this.checkBoxSymbol.UseVisualStyleBackColor = true;
+            this.checkBoxSymbol.CheckedChanged += new System.EventHandler(this.checkBoxSymbol_CheckedChanged);
+            // 
+            // checkBoxUpperCase
+            // 
+            this.checkBoxUpperCase.AutoSize = true;
+            this.checkBoxUpperCase.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxUpperCase.Location = new System.Drawing.Point(190, 74);
+            this.checkBoxUpperCase.Name = "checkBoxUpperCase";
+            this.checkBoxUpperCase.Size = new System.Drawing.Size(140, 22);
+            this.checkBoxUpperCase.TabIndex = 5;
+            this.checkBoxUpperCase.Text = "UpperCase Letters";
+            this.checkBoxUpperCase.UseVisualStyleBackColor = true;
+            this.checkBoxUpperCase.CheckedChanged += new System.EventHandler(this.checkBoxUpperCase_CheckedChanged);
+            // 
+            // checkBoxLowerCase
+            // 
+            this.checkBoxLowerCase.AutoSize = true;
+            this.checkBoxLowerCase.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxLowerCase.Location = new System.Drawing.Point(12, 74);
+            this.checkBoxLowerCase.Name = "checkBoxLowerCase";
+            this.checkBoxLowerCase.Size = new System.Drawing.Size(138, 22);
+            this.checkBoxLowerCase.TabIndex = 4;
+            this.checkBoxLowerCase.Text = "Lowercase Letters";
+            this.checkBoxLowerCase.UseVisualStyleBackColor = true;
+            this.checkBoxLowerCase.CheckedChanged += new System.EventHandler(this.checkBoxLowerCase_CheckedChanged);
+            // 
+            // button
+            // 
+            this.button.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button.Location = new System.Drawing.Point(103, 110);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(101, 34);
+            this.button.TabIndex = 6;
+            this.button.Text = "Generate";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Password";
+            // 
+            // password
+            // 
+            this.password.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.Location = new System.Drawing.Point(78, 160);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(241, 26);
+            this.password.TabIndex = 8;
+            this.password.Text = "Password will be shown here.";
+            this.password.TextChanged += new System.EventHandler(this.password_TextChanged);
+            // 
+            // list
+            // 
+            this.list.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.list.FormattingEnabled = true;
+            this.list.ItemHeight = 18;
+            this.list.Items.AddRange(new object[] {
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15"});
+            this.list.Location = new System.Drawing.Point(68, 9);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(64, 26);
+            this.list.TabIndex = 9;
+            this.list.Text = "5";
+            this.list.SelectedIndexChanged += new System.EventHandler(this.list_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 198);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.checkBoxUpperCase);
+            this.Controls.Add(this.checkBoxLowerCase);
+            this.Controls.Add(this.checkBoxSymbol);
+            this.Controls.Add(this.checkBoxNumber);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Password Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBoxNumber;
+        private System.Windows.Forms.CheckBox checkBoxSymbol;
+        private System.Windows.Forms.CheckBox checkBoxUpperCase;
+        private System.Windows.Forms.CheckBox checkBoxLowerCase;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.ComboBox list;
+    }
+}
+
---
+++ Password Generator/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password_Generator
+{
+    public partial class Form1 : Form
+    {
+        public int _length { get; set; }
+
+        public bool _number { get; set; }
+        public bool _symbol { get; set; }
+        public bool _lowerCase { get; set; }
+        public bool _upperCase { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            _length = 5;
+        }
+
+
+        private void button_Click(object sender, EventArgs e) => password.Text = GeneratePassword();
+
+        private void list_SelectedIndexChanged(object sender, EventArgs e) => _length = list.SelectedIndex + 5;
+
+        private void checkBoxNumber_CheckedChanged(object sender, EventArgs e) => _number = checkBoxNumber.Checked;
+
+        private void checkBoxSymbol_CheckedChanged(object sender, EventArgs e) => _symbol = checkBoxSymbol.Checked;
+
+        private void checkBoxLowerCase_CheckedChanged(object sender, EventArgs e) => _lowerCase = checkBoxLowerCase.Checked;
+
+        private void checkBoxUpperCase_CheckedChanged(object sender, EventArgs e) => _upperCase = checkBoxUpperCase.Checked;
+
+        private void password_TextChanged(object sender, EventArgs e) { }
+
+        private bool CheckStatus() => _number || _symbol || _lowerCase || _upperCase;
+
+        private string GeneratePassword()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (!CheckStatus()) sb.Append("Select atleast one character set!");
+            else
+            {
+                Random random = new Random();
+                int tmp = _length;
+
+            Label:
+                int index = random.Next(0, 4);
+
+                switch (index)
+                {
+                    case 0:
+                        if (_number)
+                        {
+                            sb.Append((char)random.Next(48, 58));
+                            tmp--;
+                        }
+                        break;
+                    case 1:
+                        if (_symbol)
+                        {
+                            switch (random.Next(0, 4))
+                            {
+                                case 0:
+                                    sb.Append((char)random.Next(33, 48));
+                                    break;
+                                case 1:
+                                    sb.Append((char)random.Next(58, 65));
+                                    break;
+                                case 2:
+                                    sb.Append((char)random.Next(94, 97));
+                                    break;
+                                case 3:
+                                    sb.Append((char)random.Next(123, 127));
+                                    break;
+                            }
+                            tmp--;
+                        }
+                        break;
+                    case 2:
+                        if (_lowerCase)
+                        {
+                            sb.Append((char)random.Next(97, 123));
+                            tmp--;
+                        }
+                        break;
+                    case 3:
+                        if (_upperCase)
+                        {
+                            sb.Append((char)random.Next(65, 91));
+                            tmp--;
+                        }
+                        break;
+                }
+                if (tmp > 0) goto Label;
+            }
+            return sb.ToString();
+        }
+    }
+}
